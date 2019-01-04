51bfefe987f5988b866b7bb46a9056d1c3559697
DanielGardiner <gardiner.daniel@hotmail.co.uk>
2019-01-04 13:21:55 +0000

    fixed exclude = FALSE in tab_stack_values() function

--- R/tab_stack_values.R
+++ R/tab_stack_values.R
@@ -1,4 +1,4 @@
-#' Tabulate values for multiple variables and stack ontop of one another 
+#' Tabulate values for multiple variables and stack ontop of one another
 #'
 #' @import tidyverse
 #' @author Daniel Gardiner (daniel.gardiner@phe.gov.uk)
@@ -6,7 +6,7 @@
 #' @param data a data.frame
 #' @param fill.value a value used to represent missing values
 #'
-#' @return a data.frame 
+#' @return a data.frame
 #' @export
 #'
 #' @examples
@@ -23,61 +23,61 @@
 #' # use function
 #'
 #' tab_stack_values(data, cols = c("pizza", "pasta", "salad"))
-#' 
+#'
 #' # using dplyr syntax
-#' 
+#'
 #' data %>%
 #'   select(pizza, pasta, salad) %>%
-#'   tab_stack_values() 
+#'   tab_stack_values()
 tab_stack_values = function(data, cols = NULL, fill.value = "."){
-  
+
   # convert to data.frame
-  
+
   data = as.data.frame(data)
-  
+
   # if cols argument is supplied then restrict to only those cols
-  
+
   if(is.null(cols)){
-    
+
     NULL
-    
+
   } else {
-    
+
     data = data %>%
       select_(.dots = cols)
-    
-  }
-  
-  # identify logical variables in data.frame 
-  
-  cols.logical = data %>% 
-    sapply(class) %>% 
-    as.vector() == "logical"
-  
-  cols.logical = colnames(data)[cols.logical]
-    
-  # format logical variables into character 
-  
-  temp = data %>% 
-    mutate_at(.vars = cols.logical, 
-              .funs = funs(paste0(".", as.character(.)))) %>% 
-    mutate_at(.vars = cols.logical, 
-              .funs = funs(ifelse(. == ".NA", NA, .))) %>% 
-    lapply(function(x) table(x, exclude = FALSE))
-  
 
-  # tabulate values for each variable    
-  
+  }
+
+  # identify logical variables in data.frame
+
+  cols.logical = data %>%
+    sapply(class) %>%
+    as.vector() == "logical"
+
+  cols.logical = colnames(data)[cols.logical]
+
+  # format logical variables into character
+
+  temp = data %>%
+    mutate_at(.vars = cols.logical,
+              .funs = funs(paste0(".", as.character(.)))) %>%
+    mutate_at(.vars = cols.logical,
+              .funs = funs(ifelse(. == ".NA", NA, .))) %>%
+    lapply(function(x) table(x, useNA = "ifany"))
+
+
+  # tabulate values for each variable
+
   for(i in 1:length(temp)){
     temp[[i]] = as.data.frame(temp[[i]]) %>% spread(x, Freq)
     temp[[i]]$.variable = names(temp[i])
   }
-  
-  # convert list into data.frame 
-  
-  temp %>% 
-    bind_rows() %>% 
-    select(.variable, everything()) %>% 
-    rename("variable" = ".variable") %>% 
+
+  # convert list into data.frame
+
+  temp %>%
+    bind_rows() %>%
+    select(.variable, everything()) %>%
+    rename("variable" = ".variable") %>%
     set_na_to(fill.value)
 }
